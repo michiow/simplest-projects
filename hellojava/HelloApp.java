d986d063bd2eda0ed7dc889b8067b43beea31420
Michio <michiowise@gmail.com>
2018-08-04 20:41:46 +0100

    Add hellojava

---
+++ hellojava/HelloApp.java
@@ -0,0 +1,9 @@
+/**
+ * Class HelloApp implements an application that simpley prints
+ * "Hello World! to standard output.
+ */
+class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
